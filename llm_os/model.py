d5a69538622060da8450009690aa24858fe1a050
Bohdan Agarkov <sidi0us.mail@gmail.com>
2024-07-15 14:55:52 +0200

    Installed

--- llm_os/model.py
+++ llm_os/model.py
@@ -1,10 +1,18 @@
 from typing import TypedDict, Annotated, Sequence
-from langchain_core.messages import BaseMessage
-from langcode.jupyter import Jupyter
 import operator
+
+from langcode.jupyter import Jupyter
+
+from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.messages import BaseMessage
+
+from llm_os.prompt import SYSTEM_PROMPT
+from llm_os.info import *
 
 
 class AgentState(TypedDict):
     messages: Annotated[Sequence[BaseMessage], operator.add]
     temperature: int
     jupyter: Jupyter
+
+
