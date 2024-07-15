4b9be8cc56f03ef6325af8b134cedffe5aaa7095
Bohdan Agarkov <sidi0us.mail@gmail.com>
2024-07-15 16:40:51 +0200

    Temp

--- llm_os/app.py
+++ llm_os/app.py
@@ -8,12 +8,20 @@ load_dotenv(".env")
 # On stop: stop streaming, stop jupyter notebook.
 # On chat end: close the jupyter notebook.
 
+@cl.on_chat_start
+async def on_chat_start():
+    pass
+
+@cl.on_stop
+async def on_stop():
+    pass
+
+@cl.on_chat_end
+async def on_chat_end():
+    pass
 
 @cl.on_message
 async def main(message: cl.Message):
-    # Your custom logic goes here...
-
-    # Send a response back to the user
     await cl.Message(
         content=f"Received: {message.content}",
     ).send()
